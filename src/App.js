06f09a507323059feeb29d7915bc91defc666653
matan.socher <matan.socher@zoominfo.com>
2022-06-07 13:34:13 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import { Home, Municipal, Myco, Atrea } from './components/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+        </ul> */}
+
+        <Routes>
+          <Route path="municipal" element={<Municipal />} />
+          <Route path="atrea" element={<Atrea />} />
+          <Route path="myco" element={<Myco />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
