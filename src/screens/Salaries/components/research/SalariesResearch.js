e3d342e185b6eeb92ccd3907b8716640da9920fe
matan.socher <matan.socher@zoominfo.com>
2022-08-20 00:25:58 +0300

    project structure

--- src/screens/Salaries/components/research/SalariesResearch.js
+++ src/screens/Salaries/components/research/SalariesResearch.js
@@ -10,9 +10,9 @@ function salariesResearch() {
         For that, I conducted several interviews with HR manager, as well as interviewed some of the HR coordinators. 
         I also preformed observations while they we’re working, in order to understand their work flow.</p>
         <div className="research-section-bottom">
-          <img alt="" src={salariesResearch1} alt="reasearch documents" />
-          <img alt="" src={salariesResearch2} alt="reasearch documents" />
-          {/* <img alt="" src={salariesResearch3} alt="reasearch documents" /> */}
+          <img alt="hours calculations document" src={salariesResearch1} alt="reasearch documents" />
+          <img alt="hours calculations document" src={salariesResearch2} alt="reasearch documents" />
+          {/* <img alt="hours calculations document" src={salariesResearch3} alt="reasearch documents" /> */}
         </div>
       </div>
     </section>
