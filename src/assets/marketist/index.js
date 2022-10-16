776c47e2df20932f8cdf9aca2ff46f080a5ffc5d
Socher <matan.socher@zoominfo.com>
2022-10-17 02:09:46 +0300

    work progress

--- src/assets/marketist/index.js
+++ src/assets/marketist/index.js
@@ -1,4 +1,12 @@
 import marketistExample from './marketist-example.png';
+import marketistExamples1 from './marketist-examples-1.png';
+import marketistExamples2 from './marketist-examples-2.png';
+import marketistExamples3 from './marketist-examples-3.png';
+import marketistExamples4 from './marketist-examples-4.png';
+import marketistExamples5 from './marketist-examples-5.png';
+import marketistExamples6 from './marketist-examples-6.png';
+import marketistExamples7 from './marketist-examples-7.png';
+import marketistExamples8 from './marketist-examples-8.png';
 import marketistFiles from './marketist-files.png';
 import marketistGraphs1 from './marketist-graphs-1.png';
 import marketistGraphs2 from './marketist-graphs-2.png';
@@ -11,6 +19,14 @@ import marketistTopUnder from './marketist-top-under.png';
 
 export {
   marketistExample,
+  marketistExamples1,
+  marketistExamples2,
+  marketistExamples3,
+  marketistExamples4,
+  marketistExamples5,
+  marketistExamples6,
+  marketistExamples7,
+  marketistExamples8,
   marketistFiles,
   marketistGraphs1,
   marketistGraphs2,
--- src/screens/Marketist/components/examples/MarketistExamples.js
+++ src/screens/Marketist/components/examples/MarketistExamples.js
@@ -1,24 +1,24 @@
 import './MarketistExamples.scss';
-import { marketistExample } from '../../../../assets/marketist';
+import { marketistExamples1, marketistExamples2, marketistExamples3, marketistExamples4, marketistExamples5, marketistExamples6, marketistExamples7, marketistExamples8 } from '../../../../assets/marketist';
 
 export default function MarketistExamples() {
   return (
     <section className="marketist-examples-container content">
       <div className="marketist-examples-container-box">
-        <img alt="marketist app screen" src={marketistExample} />
-        <img alt="marketist app screen" src={marketistExample} />
+        <img alt="marketist app screen" src={marketistExamples1} />
+        <img alt="marketist app screen" src={marketistExamples2} />
       </div>
       <div className="marketist-examples-container-box">
-        <img alt="marketist app screen" src={marketistExample} />
-        <img alt="marketist app screen" src={marketistExample} />
+        <img alt="marketist app screen" src={marketistExamples3} />
+        <img alt="marketist app screen" src={marketistExamples4} />
       </div>
       <div className="marketist-examples-container-box">
-        <img alt="marketist app screen" src={marketistExample} />
-        <img alt="marketist app screen" src={marketistExample} />
+        <img alt="marketist app screen" src={marketistExamples5} />
+        <img alt="marketist app screen" src={marketistExamples6} />
       </div>
       <div className="marketist-examples-container-box">
-        <img alt="marketist app screen" src={marketistExample} />
-        <img alt="marketist app screen" src={marketistExample} />
+        <img alt="marketist app screen" src={marketistExamples7} />
+        <img alt="marketist app screen" src={marketistExamples8} />
       </div>
     </section>
   );
