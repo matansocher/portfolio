b46986ee0d1f1e72276b007e42257c5543e6691f
Socher <matan.socher@zoominfo.com>
2023-04-20 11:01:38 +0300

    chore: add cache

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,7 +8,7 @@ const files = {};
 assetsConfig.forEach(asset => {
   // files[asset.name] = config.USE_CDN ? `${config.STORAGE_BASE_URL}/${asset.file}?${query_param}` : require(`./${asset.file}`);
   // files[asset.name] = `${config.STORAGE_BASE_URL}/${asset.file}`;
-  files[asset.name] = `${config.STORAGE_BASE_URL}/new/${asset.file}?${query_param}`;
+  files[asset.name] = `${config.STORAGE_BASE_URL}/new/${asset.file}`;
   // files[asset.name] = `${config.STORAGE_BASE_URL}/new/${asset.file}`;
 });
 
